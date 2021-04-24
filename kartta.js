1944ad5aa7b7a6ccbbd89ea66da55bc453cb4d42
Jarppana <jade.vartiainen@metropolia.fi>
2021-04-24 22:08:42 +0300

    Ensimmäinen versio reittikartasta
    - reittikartta sivulle
    - napit joita painamalla kartalle tulee reitin pisteet
    - tekstikenttä kartan yläpuolella, johon tulee reitin nimi.
    - kartta.js tiedostoon uusi haeReitti() - funktio

--- kartta.js
+++ kartta.js
@@ -12,3 +12,33 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 var ateneum = L.marker([60.1702332, 24.9440396]).addTo(mymap);
 ateneum.bindPopup("Ateneumin taidemuseo,<br> Kaivokatu 2, Helsinki");
 
+function haeReitti(elem) {
+  'use strict';
+  let id = elem.id - 1;
+  const url = "https://citynature.eu/api/wp/v2/places?cityid=5";
+
+  fetch(url)
+  .then(function(vastaus){
+    return vastaus.json();
+  }).then(function(json){
+    naytaTulos(json);
+  }).catch(function(error){
+    console.log(error);
+  })
+
+  function naytaTulos(json) {
+    const title = json[id].title;
+    const nimi = document.getElementById('reittinimi');
+    nimi.innerText ="Reitti: " + title;
+
+    for(let i=0;i < json[id].points.length;i++) {
+      let lat = json[id].points[i].locationPoint.lat;
+      let lng = json[id].points[i].locationPoint.lng;
+      let info = json[id].points[i].locationPoint.pointInfo;
+
+      let piste = L.marker([lat, lng]).addTo(mymap);
+      piste.bindPopup(info);
+    }
+  }
+}
+
